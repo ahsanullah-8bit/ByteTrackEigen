16bff482db5ff738153f1bb89f57bad637626c10
Ahsan Ullah <ahsanmir358@gmail.com>
2026-03-24 09:29:02 +0500

    fixed static build export definitions

--- include/Export.h
+++ include/Export.h
@@ -1,13 +1,22 @@
-#ifdef BUILDING_BYTE_TRACK_EIGEN
-#ifdef _WIN32
-#define BYTE_TRACK_EIGEN_API __declspec(dllexport)
-#else
-#define BYTE_TRACK_EIGEN_API __attribute__((visibility("default")))
+#ifndef BYTE_TRACK_EIGEN_EXPORT_H
+#define BYTE_TRACK_EIGEN_EXPORT_H
+
+#ifdef BYTE_TRACK_EIGEN_STATIC_DEFINE   // static
+#   define BYTE_TRACK_EIGEN_API
+#else                                   // dynamic/shared
+#   ifdef BUILDING_BYTE_TRACK_EIGEN
+#       ifdef _WIN32
+#           define BYTE_TRACK_EIGEN_API __declspec(dllexport)
+#       else
+#           define BYTE_TRACK_EIGEN_API __attribute__((visibility("default")))
+#       endif
+#   else
+#       ifdef _WIN32
+#           define BYTE_TRACK_EIGEN_API __declspec(dllimport)
+#       else
+#           define BYTE_TRACK_EIGEN_API __attribute__((visibility("default")))
+#       endif
+#   endif
 #endif
-#else
-#ifdef _WIN32
-#define BYTE_TRACK_EIGEN_API __declspec(dllimport)
-#else
-#define BYTE_TRACK_EIGEN_API
-#endif
+
 #endif
